2e0e515f1b28244fc762af803dae78d680242f7e
MeteAnn <meeteann@gmail.com>
2024-06-12 17:31:01 +0300

    AdoNet-Update,DELETE

--- DisconnectedMimari/Form1.Designer.cs
+++ DisconnectedMimari/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtUrunAdi = new System.Windows.Forms.TextBox();
@@ -37,20 +38,26 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnEkle = new System.Windows.Forms.Button();
             this.btnKategoriler = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudStok)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
             this.dataGridView1.Location = new System.Drawing.Point(3, 72);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(785, 366);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // label1
             // 
@@ -102,7 +109,7 @@
             // 
             // btnEkle
             // 
-            this.btnEkle.Location = new System.Drawing.Point(580, 43);
+            this.btnEkle.Location = new System.Drawing.Point(524, 40);
             this.btnEkle.Name = "btnEkle";
             this.btnEkle.Size = new System.Drawing.Size(75, 23);
             this.btnEkle.TabIndex = 7;
@@ -120,11 +127,37 @@
             this.btnKategoriler.UseVisualStyleBackColor = true;
             this.btnKategoriler.Click += new System.EventHandler(this.btnKategoriler_Click);
             // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(605, 40);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 9;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.silToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 56);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.btnKaydet);
             this.Controls.Add(this.btnKategoriler);
             this.Controls.Add(this.btnEkle);
             this.Controls.Add(this.label3);
@@ -140,6 +173,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudStok)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +190,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnEkle;
         private System.Windows.Forms.Button btnKategoriler;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
     }
 }
 
--- DisconnectedMimari/Form1.cs
+++ DisconnectedMimari/Form1.cs
@@ -20,6 +20,15 @@ namespace DisconnectedMimari
 
         SqlConnection baglanti = new SqlConnection("Server=DESKTOP-C6NAGE9\\SQLEXPRESS;Database=Northwind;Integrated security=true");
 
+        //Integrated Security : Windows Authentication ile server'a bağlanmayı sağlar.
+
+
+        //SqlConnection baglanti = new SqlConnection("Server=DESKTOP-C6NAGE9\\SQLEXPRESS;Database=Northwind; User=sa; Pwd:as");
+
+
+
+
+
         private void UrunListesi()
         {
 
@@ -28,9 +37,13 @@ namespace DisconnectedMimari
 
             DataTable dt = new DataTable(); //Bellekte verileri saklamak için kullanılan bir tablo yapısıdır. Veritabanından alınan veriler bu nesneye yüklenir.
 
-            adp.Fill(dt);
+               
+            //Tek Select sorgusu çalıştıracaksak DataSet'e gerek yoktur.
 
-            dataGridView1.DataSource = dt;
+
+            adp.Fill(dt); //Buradaki Fill doldur demektir. Yani gelen tablo çıktısını doldur diyoruz.
+
+            dataGridView1.DataSource = dt;  //Burada Veri Kaynağına tabloyu olduğu gibi veriyoruz.
 
             dataGridView1.Columns["UrunID"].Visible = false;
             dataGridView1.Columns["KategoriID"].Visible = false;
@@ -61,7 +74,7 @@ namespace DisconnectedMimari
             {
 
                 MessageBox.Show("Lütfen Geçerli Bir Değer Giriniz");
-
+                return; //burayı girmemizin sebebi uyarı verip dönmesi gerek yoksa aşağı girer çalışmaya devam eder.return ifadesi, geçersiz değerler girildiğinde metodu sonlandırmak için kullanılır. 
             }
 
             SqlCommand komut = new SqlCommand();
@@ -98,6 +111,116 @@ namespace DisconnectedMimari
             KategoriForm kf = new KategoriForm();
             kf.ShowDialog();
 
+
+
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //datagridview'Dan seçili satırı alma işlemi
+
+            txtUrunAdi.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+            nudFiyat.Value = (decimal)dataGridView1.CurrentRow.Cells[5].Value;
+
+            nudStok.Value = Convert.ToDecimal(dataGridView1.CurrentRow.Cells[6].Value);
+
+
+
+
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText ="Update Urunler set UrunAdi=@UrunAdi, BirimFiyati=@BirimFiyati,HedefStokDuzeyi=@HedefStokDuzeyi where UrunID=@UrunID";
+
+            cmd.Connection = baglanti;
+
+            cmd.Parameters.AddWithValue("@UrunID",id);
+            cmd.Parameters.AddWithValue("@UrunAdi",txtUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@BirimFiyati",nudFiyat.Value);
+            cmd.Parameters.AddWithValue("@HedefStokDuzeyi",nudStok.Value);
+
+            baglanti.Open();
+
+            
+
+
+
+            try
+            {
+                int etk = cmd.ExecuteNonQuery();
+                baglanti.Close();
+                if(etk > 0 && !string.IsNullOrEmpty(txtUrunAdi.Text))
+            {
+
+
+                    MessageBox.Show("Kayıt Güncellenmiştir.");
+                    UrunListesi();
+                    return;
+                }
+            else
+                {
+                    MessageBox.Show("Kayıt Güncellenirken Hata Oluştu");
+                }
+            }
+            catch (Exception ex)
+            {
+                baglanti.Close();
+                MessageBox.Show("Hata",ex.Message);
+                
+            }
+
+
+            
+
+            
+
+            
+
+
+
+
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow !=null)
+            {
+
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+
+                SqlCommand cmd = new SqlCommand(string.Format("delete Urunler where UrunID={0}", id), baglanti);
+                baglanti.Open();
+
+                int etk = cmd.ExecuteNonQuery();
+
+                baglanti.Close();
+
+                if (etk>0)
+                {
+
+
+                    MessageBox.Show("Kayıt Silinmiştir");
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Silinirken Hata Oluşmuştur.");
+                }
+
+
+            }
+
+            
 
 
 
